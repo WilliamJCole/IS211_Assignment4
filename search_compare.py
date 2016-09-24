4c830f85f0b19b4fd2a2947c84e1a434bf0a5af0
William Cole <billy.cole@gmail.com>
2016-09-23 23:34:48 -0400

    Commit

--- search_compare.py
+++ search_compare.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A module that contains searching algorithms."""
+"""Assignment_4 Search Algorithms."""
 
 
 import time
@@ -8,45 +8,28 @@ import random
 
 
 def sequential_search(a_list, item):
-    """Sequential Search.
-    Args:
-        a_list(list): data that is to be searched.
-        item(various): an item or value to be searched.
-    Returns:
-        (Tuple): processed time in seconds, & boolean value of items existance.
-    Examples:
-        >>> sequential_search(test_list, 42)
-        (True, 2.2172927856445312e-05)
-    """
-    start = time.time()
+    begin = time.time()
     pos = 0
     found = False
-
     while pos < len(a_list) and not found:
         if a_list[pos] == item:
             found = True
         else:
             pos = pos+1
     end = time.time()
-    return end-start, found
+    elapsed = end - begin
 
+    if not found:
+        pos = -1
+
+    return (pos, elapsed)
 
 def ordered_sequential_search(a_list, item):
-    """"Ordered Sequential Search.
-    Args:
-        a_list(list): data that is to be searched.
-        item(various): an item or value to be searched.
-    Returns:
-        (Tuple): Processed time in seconds, & boolean value of items existance.
-    Examples:
-        >>> ordered_sequential_search(test_list, 9)
-        (False, 2.9802322387695312e-05)
-    """
-    start = time.time()
+    a_list.sort()
+    begin = time.time()
     pos = 0
     found = False
     stop = False
-
     while pos < len(a_list) and not found and not stop:
         if a_list[pos] == item:
             found = True
@@ -56,28 +39,22 @@ def ordered_sequential_search(a_list, item):
             else:
                 pos = pos+1
     end = time.time()
-    return end-start, found
+    elapsed = end - begin
+
+    if not found:
+        pos = -1
+
+    return (pos, elapsed)
 
 
-def binary_search_iterative(a_list, item):
-    """Interative binary search.
-    Args:
-        a_list(list): Data that is to be searched.
-        item(various): an item or value to be searched.
-    Returns:
-        (Tuple): Processed time in seconds, & boolean value of items existance.
-    Examples:
-        >>> binary_search_iterative(test_list, 5)
-        (False, 1.71661376953125e-05)
-    """
-    start = time.time()
+def binary_search_it(a_list, item):
+    a_list.sort()
+    begin = time.time()
     first = 0
     last = len(a_list) - 1
     found = False
-
     while first <= last and not found:
         midpoint = (first + last) // 2
-
         if a_list[midpoint] == item:
             found = True
         else:
@@ -86,67 +63,72 @@ def binary_search_iterative(a_list, item):
             else:
                 first = midpoint + 1
     end = time.time()
-    return end-start, found
+    elapsed = end - begin
+
+    if not found:
+        midpoint = -1
+
+    return (midpoint, elapsed)
 
 
-def binary_search_recursive(a_list, item):
-    """Recursive binary search.
-    Args:
-        a_list(list): data that is to be searched.
-        item(various): an item or value to be searched.
-    Returns:
-        (Tuple): Processed time in seconds, & boolean value of items existance.
-    Examples:
-        >>> binary_search_recursive(test_list, 2)
-        (True, 6.9141387939453125e-06)
-    """
-    start = time.time()
-    if len(a_list) == 0:
-        found = False
-    else:
-        midpoint = len(a_list) // 2
+def binary_search_rec(a_list, item):
+
+    def binary_search_helper(a_list, item, begin_time, start, end):
+        if start > end:
+            return (-1, time.time() - begin)
+
+        midpoint = (start + end) // 2
+
         if a_list[midpoint] == item:
-            found = True
+            return (midpoint, time.time() - begin)
         else:
             if item < a_list[midpoint]:
-                return binary_search_recursive(a_list[:midpoint], item)
+                return binary_search_helper(a_list, item, begin_time, start,
+                                            midpoint - 1)
             else:
-                return binary_search_recursive(a_list[midpoint + 1:], item)
-    end = time.time()
-    return end-start, found
+                return binary_search_helper(a_list, item, begin_time,
+                                            midpoint + 1, end)
 
+    a_list.sort()
+    begin = time.time()
+    return binary_search_helper(a_list, item, begin, 0, len(a_list))
 
-def random_list(avg_time):
-    """Generator for a list of algorithm searches."""
-    my_list = []
-    for item in range(avg_time):
-        my_list.append(random.randint(1, avg_time))
-    return my_list
-
+def generate_random_list(size):
+    the_list = []
+    for i in range(size):
+        the_list.append(random.randint(0, 10000))
+    return the_list
 
 def main():
-    """The main function of the program."""
-    test_num = [500, 1000, 10000]
+    num_list = 100
+    sort_functions = [(sequential_search, "Sequential Search"),
+                      (ordered_sequential_search, "Ordered Sequential Search"),
+                      (binary_search_it, "Iterative Binary Search"),
+                      (binary_search_rec, "Recursive Binary Search")]
+    list_sizes = [500, 1000, 10000]
 
-    for i in test_num:
-        counter = 100
-        results = [0, 0, 0, 0]
-        while counter > 0:
-            my_list = random_list(i)
-            results[0] += sequential_search(my_list, -1)[0]
-            results[1] += ordered_sequential_search(my_list, -1)[0]
-            results[2] += binary_search_iterative(my_list, -1)[0]
-            results[3] += binary_search_recursive(my_list, -1)[0]
-            counter -= 1
-        print 'For the list of {}... '.format(i)
-        print ('The sequential search took %10.7f seconds to run, '
-                'on average.') % (results[0] / 100)
-        print ('The ordered sequential search ' + 'took %10.7f seconds to run, '
-                'on average.') % (results[1] / 100)
-        print ('The iterative binary search ' + 'took %10.7f seconds to run, '
-                'on average.') % (results[2] / 100)
-        print ('The recursive binary search ' + 'took %10.7f seconds to run, '
-                'on average.') % (results[3] / 100)
+    for list_size in list_sizes:
 
-if __name__ == "__main__":
+        sum_of_search_time_list = []
+
+        for i in range(len(sort_functions)):
+            sum_of_search_time_list.append(0.0)
+
+        print("List of size %d:"%(list_size))
+
+        for i in range(num_list):
+
+            a_list = generate_random_list(list_size)
+
+            for j, function_tuple in enumerate(sort_functions):
+                function, name = function_tuple
+                result, duration = function(a_list, -1)
+                sum_of_search_time_list[j] += duration
+
+        for j, function_tuple in enumerate(sort_functions):
+            function, name = function_tuple
+            print("\t%s took %10.7f seconds to run, on average"%(name,
+                 (sum_of_search_time_list[j]/num_list)))
+
+if __name__ == '__main__':
     main()
--- sort_compare.py
+++ sort_compare.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A module that contains sort algorithms."""
+"""Assignment_4 Sort_Compare."""
 
 
-import time
 import random
-
+import time
 
 def insertion_sort(a_list):
-    """Insertion list sort function."""
-    start = time.time()
+    """Insetion Sort:
+
+    Args:
+        a_list (list): list of random numbers.
+
+    Returns:
+        elapsed (float): Function exec time.
+    """
+
+    begin = time.time()
     for index in range(1, len(a_list)):
+
         current_value = a_list[index]
         position = index
+
         while position > 0 and a_list[position - 1] > current_value:
             a_list[position] = a_list[position - 1]
             position = position - 1
-        a_list[position] = current_value
-    end = time.time()
-    return end-start, a_list
 
+        a_list[position] = current_value
+
+    return time.time() - begin
 
 def gap_insertion_sort(a_list, start, gap):
-    """Gap insertion sort function."""
-
     for i in range(start + gap, len(a_list), gap):
         current_value = a_list[i]
         position = i
@@ -32,56 +39,70 @@ def gap_insertion_sort(a_list, start, gap):
             position = position - gap
         a_list[position] = current_value
 
-
 def shell_sort(a_list):
-    """Shell list sort function."""
-    start = time.time()
+    """Shell Sort:
+
+    Args:
+        a_list (list): list of random numbers.
+
+    Returns:
+        elapsed (float): Function exec time.
+   """
+    begin = time.time()
+
     sublist_count = len(a_list) // 2
     while sublist_count > 0:
         for start_position in range(sublist_count):
             gap_insertion_sort(a_list, start_position, sublist_count)
-        sublist_count = sublist_count // 2
-    end = time.time()
-    return end-start, a_list
 
+        sublist_count = sublist_count // 2
+    return time.time() - begin
 
 def python_sort(a_list):
-    """List sort function."""
-    start = time.time()
-    a_list = a_list.sort()
-    end = time.time()
-    return end-start, a_list
+    """Python Sort:
 
+    Args:
+        a_list (list): list of random numbers.
 
-def random_list(avg_time):
-    """Random list generator."""
-    my_list = []
-    for item in range(avg_time):
-        my_list.append(random.randint(1,avg_time))
-    return my_list
+    Returns:
+        elapsed (float): Function exec time.
+   """
+    begin = time.time()
+    a_list.sort()
+    return time.time() - begin
 
+def generate_random_list(size):
+    the_list = []
+    for i in range(size):
+        the_list.append(random.randint(0,10000))
+    return the_list
 
-def main():
-    """The main function of the program."""
-    test_num = [500, 1000, 10000]
+if __name__ == '__main__':
 
-    for i in test_num:
-        counter = 100
-        result = [0, 0, 0]
+    num_list = 100
+    sort_functions = [(insertion_sort,"Insertion Sort"), (shell_sort,"Shell Sort"),
+                         (python_sort,"Python Sort")]
+    list_sizes = [500,1000,10000]
 
-        while counter > 0:
-            my_list = random_list(i)
-            result[0] += insertion_sort(my_list)[0]
-            result[1] += shell_sort(my_list)[0]
-            result[2] += python_sort(my_list)[0]
-            counter -= 1
-        print 'For the list of {}... '.format(i)
-        print ('Insertion Sort took %10.7f seconds to run,'
-               'on average.' % (result[0] / 100))
-        print ('Shell Sort ' + 'took %10.7f seconds to run,'
-               'on average.' % (result[1] / 100))
-        print ('Python Sort ' + 'took %10.7f seconds to run,'
-               'on average.' % (result[2] / 100))
+    for list_size in list_sizes:
 
-if __name__ == "__main__":
-    main()
+        sum_of_search_time_list = []
+
+        for i in range(len(sort_functions)):
+            sum_of_search_time_list.append(0.0)
+
+        print("List of size %d:"%(list_size))
+
+        for i in range(num_list):
+
+            a_list = generate_random_list(list_size)
+
+            for j, function_tuple in enumerate(sort_functions):
+                function, name = function_tuple
+                list_copy = a_list[:]
+                duration = function(list_copy)
+                sum_of_search_time_list[j] += duration
+
+        for j, function_tuple in enumerate(sort_functions):
+            function, name = function_tuple
+            print("\t%s took %10.7f seconds to run, on average"%(name, (sum_of_search_time_list[j]/num_list)))
